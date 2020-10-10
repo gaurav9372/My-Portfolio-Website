fb06a99baa6c3ec903e33e31ad30f3d6260ef890
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-10-10 20:21:41 +0530

    Tasky Added

---
+++ works/projects/Tasky/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    
+    
+$(function openGroup(){
+$(".groupHeader").click(function(){
+    
+    $(this).next().toggleClass("active");
+    
+});
+});
+    
+$(function swapClick(){
+$(".beforeClick").click(function(){
+    
+    $(this).removeClass("active");
+    $(this).next().addClass("active");
+    
+});
+});
+    
+      
+$(function AddGroup(){
+    $("#doneButton").click(function(){
+        
+var groupName = $("#newGroupName").val();
+        
+if(groupName !== ''){
+            
+var groupTitle = 
+$("<p></p>").addClass("groupTitle").text(groupName);    
+    
+var groupHeader =
+$("<div></div>").addClass("groupHeader").append(groupTitle);           
+    
+var newGroup = 
+$("<div></div>").addClass("group").append(groupHeader);
+            
+            
+$(".newGroup").before(newGroup);
+$("#newGroupName").val("");
+        }
+else{
+    alert("Enter Group name please");
+};
+        
+$(".afterClick").removeClass("active");
+$(".beforeClick").addClass("active");
+        
+    });
+});
+    
+    
+    
+    
+    
+    
+    
+    
+});
+
+
