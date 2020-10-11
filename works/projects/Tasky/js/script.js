2b1224e385e4f7bf42d91879e67df9bbce1c845f
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-10-11 17:40:39 +0530

    ik

--- works/projects/Tasky/js/script.js
+++ works/projects/Tasky/js/script.js
@@ -1,41 +1,73 @@
 $(document).ready(function(){
     
     
-$(function openGroup(){
-$(".groupHeader").click(function(){
     
-$(this).next().toggleClass("active");
-$(this).find(".arrowIcon").toggleClass("active");
     
-});
-});
-    
+
 $(function swapClick(){
 $(".beforeClick").click(function(){
     
     $(this).removeClass("active");
     $(this).next().addClass("active");
+    $(this).next().find("input").focus();
     
 });
 });
+        
+$(function Additem(){
     
-      
-$(function AddGroup(){
-    $("#doneButton").click(function(){
+$(".taskSwap .doneButton").click(function(){
+        
+var taskName = $("#newTaskName").val();
+              
+if(taskName !== ''){
+            
+var taskTitle = 
+$("<p></p>").addClass("taskTitle").text(taskName);      
+var checkbox = $("<input>").attr("type","checkbox");
+    
+ 
+var newTask = 
+$("<div></div>").addClass("task").append(checkbox,taskTitle);
+            
+$(".newTask").before(newTask);
+    
+$("#newTaskName").val("");
+}
+        
+else{
+    alert("Enter task");
+};
+        
+$(".afterClick").removeClass("active");
+$(".beforeClick").addClass("active");
+        
+    });
+    
+    
+    
+$(".groupSwap .doneButton").click(function(){
         
 var groupName = $("#newGroupName").val();
         
 if(groupName !== ''){
             
 var groupTitle = 
-$("<p></p>").addClass("groupTitle").text(groupName);    
+$("<p></p>").addClass("groupTitle").text(groupName);   
+var icon = $("<img>").attr("src","resources/Icons/arrow.svg");
+    
+var arrowIcon =
+$("<div></div>").addClass("arrowIcon").append(icon);
     
 var groupHeader =
-$("<div></div>").addClass("groupHeader").append(groupTitle);           
+$("<div></div>").addClass("groupHeader").append(groupTitle,arrowIcon); 
     
+var groupContent =
+$("<div></div>").addClass("groupContent").append("<h2>YEs</h2>");           
+
 var newGroup = 
-$("<div></div>").addClass("group").append(groupHeader);
-            
+$("<div></div>").addClass("group").append(groupHeader,groupContent);
+      
             
 $(".newGroup").before(newGroup);
 $("#newGroupName").val("");
@@ -48,7 +80,30 @@ $(".afterClick").removeClass("active");
 $(".beforeClick").addClass("active");
         
     });
+    
+    
 });
+    
+$(function openGroup(){
+$(".groupHeader").click(function(){
+    
+$(this).next(".groupContent").toggleClass("active");
+$(this).find(".arrowIcon").toggleClass("active");
+    
+});
+});
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
