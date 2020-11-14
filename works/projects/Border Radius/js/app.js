2399753ae8afc1b34bfd1bc5803ae1f9870011c6
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-11-15 00:03:31 +0530

    kjl

--- works/projects/Border Radius/js/app.js
+++ works/projects/Border Radius/js/app.js
@@ -4,9 +4,28 @@ new Vue({
     
     data:{
         
-        roundness: 80,
+        roundnessOne:"30",
         
+        roundnessTwoA:"20",
+        roundnessTwoB:"180",
         
+        roundnessTwoA_s:"60",
+        roundnessTwoB_s:"140",
+        
+        roundnessThreeA:"20",
+        roundnessThreeB:"150",
+        roundnessThreeC:"90",
+        
+        roundnessThreeA_s:"20",
+        roundnessThreeB_s:"150",
+        roundnessThreeC_s:"90",
+        
+        roundnessFourA:"0",
+        roundnessFourB:"0",
+        roundnessFourC:"200",
+        roundnessFourD:"200",
+        
+         
     },
     
     computed:{
