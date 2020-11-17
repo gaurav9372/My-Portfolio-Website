c8907d392b3c752bf3897b4f137bd3c93486a431
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-11-17 17:59:59 +0530

    added

--- works/projects/Border Radius Visualized/js/app.js
+++ works/projects/Border Radius Visualized/js/app.js
@@ -1,11 +1,18 @@
 new Vue({
     
     el:'#app', 
-    
+     
     data:{
          
     roundness50:"15",
     roundness100:"15",
+    roundness200:"15",
+    roundness400:"15",
+        
+    dyHeight:"200",
+    dyWidth:"200",
+        
+    
     visible:true,
     },
 
@@ -22,7 +29,22 @@ new Vue({
             height: this.roundness100 * 2 + 'px',
             width: this.roundness100 * 2 + 'px',
         }
-    }
+    },
+    guideCircles200: function () {
+        return {
+            height: this.roundness200 * 2 + 'px',
+            width: this.roundness200 * 2 + 'px',
+        }
+    },
+    guideCircles400: function () {
+        return {
+            height: this.roundness400 * 2 + 'px',
+//            height: this.dyHeight+ 'px',
+            width: this.roundness400 * 2 + 'px',
+        }
+    },
+        
+    
 },
     methods:{
 
